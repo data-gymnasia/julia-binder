ae1f8d5ef3e8ec9650a05b15447707feb40267f1
Samuel Watson <samuel.s.watson@gmail.com>
2020-05-04 16:45:56 -0400

    Update simulation script

--- data-gymnasia/sir-simulation.jl
+++ data-gymnasia/sir-simulation.jl
@@ -1,9 +1,10 @@
 using Plots
 import Base.Iterators: countfrom
-
+ 
 function SIR_simulation(population_size, infection_probability)
     statuses = fill("susceptible", population_size, 1)
     statuses[1, 1] = "infectious"
+    transmissions = []
     for t in countfrom(2)
         n_infectious = sum(statuses[:, t-1] .== "infectious")
         if n_infectious == 0
@@ -11,14 +12,22 @@ function SIR_simulation(population_size, infection_probability)
         end
         statuses = [statuses fill("susceptible", population_size)]
         for k in 1:population_size
-            if statuses[k, t-1] == "susceptible"
-                if rand() < 1 - (1 - infection_probability)^n_infectious
-                    statuses[k, t] = "infectious"
-                end
-            else
+            if statuses[k, t-1] == "recovered" || statuses[k, t-1] == "infectious"
                 statuses[k, t] = "recovered"
             end
         end
+        for j in 1:population_size
+            if statuses[j, t-1] == "infectious"
+                for k in 1:population_size
+                    if statuses[k, t-1] == "susceptible"
+                        if rand() < infection_probability
+                            push!(transmissions, [(j, t-1),(k, t)])
+                            statuses[k, t] = "infectious"
+                        end
+                    end
+                end
+            end
+        end
     end
-    statuses
+    statuses, transmissions
 end
